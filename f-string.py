ac7107c2be556fdd3b2a1d0dc48c1e32c7c82296
BLIGHT <shaming000731@gmail.com>
2025-06-04 14:08:28 +0545

    Update f-string.py

--- f-string.py
+++ f-string.py
@@ -1,3 +1,3 @@
 name = "Nishaim Limbu"
 age = 18
-print (f"My name is {name}.\nI am {age} years old.")
+print (f"My name is {name}.\nI am {age} years old.")#f is used to print value in certain place.
