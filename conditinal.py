bb734e4084711909560de3f180a0a4702143b701
BLIGHT <shaming000731@gmail.com>
2025-06-04 13:58:53 +0545

    Update conditinal.py

--- conditinal.py
+++ conditinal.py
@@ -3,7 +3,7 @@ Smoke=(input ("Enter Yes if he/she smokes else No:"))
 Exercie=(input ("Enter Yes if he/she do exercise else No:"))
 if age>50 and Smoke=="yes" and Exercie=="no":
     print("High Risk of Heart Disease")
-elif age>50 or Smoke=="yes" and Exercise=="no":
+elif (age>50 or Smoke=="yes") and Exercise=="no":#works or for inside () and check and for others
     print("Midium Risk of Heart Disease")
 else:
     print("Low Risk of Heart Disease") 
